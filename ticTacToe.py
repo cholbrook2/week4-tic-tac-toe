649d4c2c124db6d4ccaf34ea449191f8e9d0dd6e
Chelsea Holbrook <cholbrook2@mail.valenciacollege.edu>
2016-02-09 12:41:50 -0500

    Week 4 and 5
    Tic-tac-toe!

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,4 +1,15 @@
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
 def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+printBoard(theBoard)
+
     # TO DO #################################################################
     # Write code in this function that prints the game board.               #
     # The code in this function should only print, the user should NOT      #
@@ -7,9 +18,18 @@ def printBoard(board):
     # Hint: you can follow the same process that was done in the textbook.  #
     #########################################################################
 
-def checkWinner(board, player):    
+def checkWinner(board, player):
     print('Checking if ' + player + ' is a winner...')
-    
+    return((board['top-L'] == player and board['top-M'] == player and board['top-R'] == player) or #top horizonta
+        (board['mid-L'] == player and board['mid-M'] == player and board['mid-R'] == player) or #mid horizontal
+        (board['low-L'] == player and board['low-M'] == player and board['low-R']== player) or #low horizontal
+        (board['top-L'] == player and board['mid-L'] == player and board['low-L'] == player) or #left vertical
+        (board['top-M'] == player and board['mid-M'] == player and board['low-M']== player) or #mid vertical
+        (board['top-R'] == player and board['mid-R'] == player and board['low-R']== player) or #right vertical
+        (board['top-L'] == player and board['mid-M'] == player and board['low-R']== player) or #diag left to right
+        (board['top-R'] == player and board['mid-M'] == player and board['low-L']== player)) #diag right to left
+
+
     # TO DO #################################################################
     # Write code in this function that checks the tic-tac-toe board          #
     # to determine if the player stored in variable 'player' currently      #
@@ -18,31 +38,31 @@ def checkWinner(board, player):
     # variable 'player' has won. The function should return False           #
     # if the player in the variable 'player' has not won.                   #
     #########################################################################
-    
-    
+
+
 def startGame(startingPlayer, board):
     # TO DO #################################################################
     # Add comments to each line in this function to describe what           #
     # is happening. You do not need to modify any of the Python code        #
     #########################################################################
 
-    turn = startingPlayer
+    turn = startingPlayer #labeling turn for first player
+
     for i in range(9):
-        printBoard(board)
-        print('Turn for ' + turn + '. Move on which space?')
-        move = input()
-        board[move] = turn
-        if( checkWinner(board, 'X') ):
+        printBoard(board) #prints gameboard
+        print('Turn for ' + turn + '. Move on which space?') #ask where to play
+        move = input() #giving value of input to variable
+        board[move] = turn #update board
+        if( checkWinner(board, 'X') ): #final win move for X
             print('X wins!')
             break
-        elif ( checkWinner(board, 'O') ):
+        elif ( checkWinner(board, 'O') ): #final win move for O
             print('O wins!')
             break
-    
-        if turn == 'X':
+
+        if turn == 'X': #switch to O
             turn = 'O'
-        else:
+        else:           #switch to X
             turn = 'X'
-        
+
     printBoard(board)
-    
